0dca45a7e894b342a5a5eb50c34c45b069538e0f
Johnson Alala <jonesalala95@gmail.com>
2025-08-03 17:34:02 +0300

    Migrations and alembic

---
+++ app/migrations/versions/a0f63075769c_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: a0f63075769c
+Revises: 
+Create Date: 2025-08-03 17:06:10.730890
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a0f63075769c'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
